2d683657d70dd0b13d819056ab78f4b5e2786870
Tomas13d <tomas.demobio@gmail.com>
2023-03-10 00:24:11 -0300

    feat: mobile ready

--- src/components/galery/mobile/index.jsx
+++ src/components/galery/mobile/index.jsx
@@ -2,63 +2,50 @@ import { useEffect, useRef, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import { sortedJson, allCategories } from "../../../utils/galery";
 import FilterDropdown from "../desktop/filterDropdown";
-import { filterProyects } from "../desktop/sortFunctions";
+import { filterObjectsByTags, filterProyects } from "../desktop/sortFunctions";
 import "./galeryMobile.css";
 
-function GaleryMobile({ secondTitle}) {
+function GaleryMobile({ secondTitle }) {
   const [proyects, setProyects] = useState([]);
-  const [categories, setCategories] = useState([""]);
+  const [allProyects, setAllProyects] = useState([]);
   const [segmentedProjects, setSegmentedProjects] = useState([]);
-  const [activeFilter, setActiveFilters] = useState([]);
+  const [categories, setCategories] = useState([""]);
   const [showFilterNav, setShowFilterNav] = useState(false);
+  const [activeFilter, setActiveFilters] = useState([]);
   const [showMore, setShowMore] = useState(5);
-  const [inLast, setInLast] = useState(false);
-  const lastImage = useRef(null);
-  const amountOfCards= useRef(5) 
+  const amountOfCards = useRef(5);
 
   useEffect(() => {
-    if (showMore === 5) {
-      setSegmentedProjects(proyects.slice(0, showMore));
-    } else {
-      if (showMore >= proyects.length ) {
-        setSegmentedProjects(proyects);
-      }
-      setSegmentedProjects(
-        segmentedProjects.concat(proyects.slice(showMore, showMore + amountOfCards.current))
-      );
-    }
-
-    if (lastImage.current !== null && lastImage.current.dataset.position) {
-      setInLast(parseInt(lastImage.current.dataset.position)+amountOfCards.current);
-    }
-  }, [showMore, proyects]);
+    showMore === 5
+      ? setSegmentedProjects(proyects.slice(0, showMore))
+      : showMore >= proyects.length
+      ? setSegmentedProjects(proyects)
+      : setSegmentedProjects(
+          segmentedProjects.concat(
+            proyects.slice(showMore, showMore + amountOfCards.current)
+          )
+        );
+  }, [proyects, showMore]);
 
   useEffect(() => {
     if (sortedJson) {
       setCategories(allCategories);
       setProyects(sortedJson);
+      setAllProyects(sortedJson);
     }
   }, [sortedJson]);
 
   const handleChange = (e) => {
     const input = e.currentTarget;
     const filters = activeFilter;
-    if (input.checked) {
-      filters.push(input.value);
-    } else {
-      filters.splice(filters.indexOf(input.value), 1);
-      
-    }
-    if (filters.length !== 0){
-      const filteredProducts = filterProyects(proyects,filters)
-      console.log("filtered -->", filteredProducts);
-      setProyects(filteredProducts)
-    } else {
-      setProyects(sortedJson)
-    }
-    setInLast(false);
+    input.checked
+      ? filters.push(input.value)
+      : filters.splice(filters.indexOf(input.value), 1);
+    setActiveFilters(filters);
+    filters.length !== 0
+      ? setProyects(filterObjectsByTags(allProyects, filters))
+      : setProyects(allProyects);
     setShowMore(5);
-     
   };
 
   return (
@@ -68,7 +55,13 @@ function GaleryMobile({ secondTitle}) {
       </div>
 
       <div className="col-12 d-flex justify-content-between aling-items-center">
-        <FilterDropdown categories={categories} handleChange={handleChange} show={showFilterNav} setShow={setShowFilterNav} mobileSize={true}/>
+        <FilterDropdown
+          categories={categories}
+          handleChange={handleChange}
+          show={showFilterNav}
+          setShow={setShowFilterNav}
+          mobileSize={true}
+        />
         <h6 className="ff-circularBold mobile-second-title">{secondTitle}</h6>
       </div>
 
@@ -76,8 +69,6 @@ function GaleryMobile({ secondTitle}) {
         {segmentedProjects.map((proyect, i) => (
           <div
             className={`mobile-galery-card`}
-            ref={lastImage}
-            data-position={i}
             key={i + new Date().getTime}
           >
             <img
@@ -96,7 +87,7 @@ function GaleryMobile({ secondTitle}) {
             </div>
           </div>
         ))}
-        {inLast !== proyects.length - 1 ? (
+        {segmentedProjects.length !== proyects.length ? (
           <div
             className={`see-more-card`}
             onClick={() => setShowMore(showMore + amountOfCards.current)}

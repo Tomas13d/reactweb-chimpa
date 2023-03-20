889493ed1aa8c31014d7b4bf28e0e22280c504cd
Tomas13d <tomas.demobio@gmail.com>
2023-03-19 22:11:41 -0300

    feat: modal blur off

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,70 +1,135 @@
 import { useEffect, useRef, useState } from "react";
 import { Container, Navbar } from "react-bootstrap";
-import '@dotlottie/player-component';
+import "@dotlottie/player-component";
 import ChimpanceLogo from "../../assets/illustrations/ChimpanceLogo";
 import ImgIsoOjos from "../../assets/illustrations/ImgIsoOjos";
-import lottie from "../../assets/lotties/bg-jungla-desk.lottie"
-import "./navbar.css"
+import lottie from "../../assets/lotties/bg-jungla-desk.lottie";
+import "./navbar.css";
 
 function NavbarApp() {
-    const [activeMenu, setActiveMenu] = useState(false)
-    const [navBackground, setNavBackground] = useState(false)
-    const navRef = useRef()
-    navRef.current = navBackground
-    useEffect(() => {
-      const handleScroll = () => {
-        if(navBackground){
-          return
-        }
-        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.getElementById("navbar").classList.add('background-filter');
-        } else {
-            document.getElementById("navbar").classList.remove('background-filter');
-        }
+  const [activeMenu, setActiveMenu] = useState(false);
+  useEffect(() => {
+    if (activeMenu)
+      document.getElementById("navbar").classList.remove("background-filter");
+  }, [activeMenu]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        (document.body.scrollTop > 80 ||
+          document.documentElement.scrollTop > 80) &&
+        !activeMenu
+      ) {
+        document.getElementById("navbar").classList.add("background-filter");
+      } else {
+        document.getElementById("navbar").classList.remove("background-filter");
       }
-      document.addEventListener('scroll', handleScroll)
-      return () => {
-        document.removeEventListener('scroll', handleScroll)
-      }
-    }, [])
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  });
 
   return (
     <>
-    <nav id="navbar" className={`navbar custom-nav `}>
+      <nav id="navbar" className={`navbar custom-nav`}>
         <Container fluid className="container position-relative">
-            <Navbar.Brand href="/" className="d-none d-md-block"><ChimpanceLogo/></Navbar.Brand>
-            <Navbar.Brand href="/" className="col-12 d-flex justify-content-center d-md-none"><ImgIsoOjos customClass="mobile-eyes"/></Navbar.Brand>
-            <div className="d-flex align-items-center">
-                <a id="contact_nav" rel="noreferrer" href="https://web.whatsapp.com/send?phone=+5493516503470&amp;text=Buenos%20días,%20quiero%20mas%20info%20sobre%20desarrollo%20web" target="_blank"  className={`btn btn-white d-none ${activeMenu ? "" : "d-md-block"}`}>Contactanos</a>
-                <div className="d-none d-md-block">
-                    <input type="checkbox" className="toggler" id="toggleButton" onClick={() => setActiveMenu(!activeMenu)}/>
-                    <div className={`hamburger ${activeMenu ? "checked" : ""}`} id="hamburger"><div></div></div>
-                </div>
+          <Navbar.Brand href="/" className="d-none d-md-block">
+            <ChimpanceLogo />
+          </Navbar.Brand>
+          <Navbar.Brand
+            href="/"
+            className="col-12 d-flex justify-content-center d-md-none"
+          >
+            <ImgIsoOjos customClass="mobile-eyes" />
+          </Navbar.Brand>
+          <div className="d-flex align-items-center">
+            <a
+              id="contact_nav"
+              rel="noreferrer"
+              href="https://web.whatsapp.com/send?phone=+5493516503470&amp;text=Buenos%20días,%20quiero%20mas%20info%20sobre%20desarrollo%20web"
+              target="_blank"
+              className={`btn btn-white d-none ${
+                activeMenu ? "" : "d-md-block"
+              }`}
+            >
+              Contactanos
+            </a>
+            <div className="d-none d-md-block">
+              <input
+                type="checkbox"
+                className="toggler"
+                id="toggleButton"
+                onClick={() => setActiveMenu((activeMenu) => !activeMenu)}
+              />
+              <div
+                className={`hamburger ${activeMenu ? "checked" : ""}`}
+                id="hamburger"
+              >
+                <div></div>
+              </div>
             </div>
+          </div>
         </Container>
-    </nav>
-    <Container className="menu-wrap nav-mobile">
+      </nav>
+      <Container className="menu-wrap nav-mobile">
         <div className={`menu ${activeMenu ? "checked" : ""}`} id="menu">
-            <div>
-                <div id="bg-video" className="d-none d-md-block video-container">
-                        <dotlottie-player id="nav-deskLottie" autoplay loop  mode="normal" src={lottie} background="transparent" style={{width: "100vw", height: "100vh", zIndex:0, transform: "scale(1.3)"}}/>
-                </div>
-                <div id="bg-video" className="d-block d-md-none video-container" >
-                        <dotlottie-player autoplay loop  mode="normal" src={lottie} background="transparent" style={{width: "100vw", height: "100vh", zIndex:0, transform: "scale(1.3)"}}/>
-                </div>
-                <div>
-                    <ul className="p-0">
-                        <li className="mb-4"><ImgIsoOjos customClass="eyes-illustration"/></li> 
-                        <li><a className="ff-circularBold" href="desarrollo-web.php">Web UX UI</a></li>
-                        {/* <li><a className="ff-circularBold" target="_blank"  href="https://chimpance.digital/branding">Branding</a></li> 
-                        <li><a className="ff-circularBold" target="_blank"  href="./">Mkt Digital</a></li>  */}
-                        <li><a className="ff-circularBold" href="#form" id="link">Contacto</a></li>
-                        </ul>
-                </div>
+          <div>
+            <div id="bg-video" className="d-none d-md-block video-container">
+              <dotlottie-player
+                id="nav-deskLottie"
+                autoplay
+                loop
+                mode="normal"
+                src={lottie}
+                background="transparent"
+                style={{
+                  width: "100vw",
+                  height: "100vh",
+                  zIndex: 0,
+                  transform: "scale(1.3)",
+                }}
+              />
             </div>
+            <div id="bg-video" className="d-block d-md-none video-container">
+              <dotlottie-player
+                autoplay
+                loop
+                mode="normal"
+                src={lottie}
+                background="transparent"
+                style={{
+                  width: "100vw",
+                  height: "100vh",
+                  zIndex: 0,
+                  transform: "scale(1.3)",
+                }}
+              />
+            </div>
+            <div>
+              <ul className="p-0">
+                <li className="mb-4">
+                  <ImgIsoOjos customClass="eyes-illustration" />
+                </li>
+                <li>
+                  <a className="ff-circularBold" href="desarrollo-web.php">
+                    Web UX UI
+                  </a>
+                </li>
+                {/* <li><a className="ff-circularBold" target="_blank"  href="https://chimpance.digital/branding">Branding</a></li> 
+                        <li><a className="ff-circularBold" target="_blank"  href="./">Mkt Digital</a></li>  */}
+                <li>
+                  <a className="ff-circularBold" href="#form" id="link">
+                    Contacto
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-    </Container>
-</>
+      </Container>
+    </>
   );
 }
 
